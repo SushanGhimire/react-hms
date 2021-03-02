725b314afc6e03ceb642b98e9d912503f6f62157
SushanGhimire <sushangmi55@gmail.com>
2021-03-02 11:52:42 +0545

    animation off

--- src/component/Home.js
+++ src/component/Home.js
@@ -8,7 +8,7 @@ import RecentBlog from "./blogs/RecentBlog";
 import Footer from "./Footer";
 const Home = () => {
   return (
-    <section className="w-screen h-screen flex flex-col ">
+    <section className="w-screen h-screen flex flex-col overflow-x-hidden">
       <main
         className="w-full min-h-screen flex flex-col  bg-fixed"
         style={{
